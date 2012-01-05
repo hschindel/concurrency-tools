107d834a450f86d6f1dda232684daa18558d1b1f
Howard Schindel <howard.schindel@gmail.com>
2012-01-04 22:36:00 -0500

    Build out of mutable methods.

--- cctools/src/main/java/com/cctools/atomic/AtomicBigInteger.java
+++ cctools/src/main/java/com/cctools/atomic/AtomicBigInteger.java
@@ -2,15 +2,17 @@ package com.cctools.atomic;
 
 import java.math.BigInteger;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author howard.schindel
  * 
- * This is an immutable atomic version of BigInteger. 
+ * This is an mutable atomic version of BigInteger.
  *
  */
 public class AtomicBigInteger extends Number {
+	AtomicInteger i;
 
 	/**
 	 * 
@@ -20,6 +22,7 @@ public class AtomicBigInteger extends Number {
 	private AtomicReference<BigInteger> bigInt;
 	
 	private static final BigInteger ONE = new BigInteger("1");
+	private static final BigInteger NEGATIVE_ONE = new BigInteger("-1");
 	
 	public AtomicBigInteger(BigInteger bigInt) {
 		this.bigInt = new AtomicReference<BigInteger>(bigInt);
@@ -43,8 +46,7 @@ public class AtomicBigInteger extends Number {
 	
 	public AtomicBigInteger(String val, int radix) {
 		bigInt = new AtomicReference<BigInteger>(new BigInteger(val, radix));
-	}
-	
+	}	
 	
 	@Override
 	public double doubleValue() {
@@ -66,44 +68,128 @@ public class AtomicBigInteger extends Number {
 		return bigInt.get().longValue();
 	}
 	
-	public BigInteger addAndGet(BigInteger delta) {
+	public final void set(BigInteger newValue) {
+		bigInt.set(newValue);
+	}
+	
+	public final BigInteger addAndGet(BigInteger delta) {
 		while(true) {
 			BigInteger currentVal = bigInt.get();
-			BigInteger nextVal = bigInt.get().add(delta);
+			BigInteger nextVal = currentVal.add(delta);
 			if(bigInt.compareAndSet(currentVal, nextVal)) {
 				return nextVal;
 			}
 		}
 	}
 	
-	public BigInteger getAndAdd(BigInteger delta) {
+	public final BigInteger getAndAdd(BigInteger delta) {
 		while(true) {
 			BigInteger currentVal = bigInt.get();
-			BigInteger nextVal = bigInt.get().add(delta);
+			BigInteger nextVal = currentVal.add(delta);
 			if(bigInt.compareAndSet(currentVal, nextVal)) {
 				return currentVal;
 			}
 		}
 	}
 
-	public BigInteger getAndIncrement() {
+	public final BigInteger getAndIncrement() {
 		while(true) {
 			BigInteger currentVal = bigInt.get();
-			BigInteger nextVal = bigInt.get().add(ONE);
+			BigInteger nextVal = currentVal.add(ONE);
 			if(bigInt.compareAndSet(currentVal, nextVal)) {
 				return currentVal;
 			}
 		}
 	}
 	
-	public BigInteger incrementAndGet() {
+	public final BigInteger incrementAndGet() {
 		while(true) {
 			BigInteger currentVal = bigInt.get();
-			BigInteger nextVal = bigInt.get().add(ONE);
+			BigInteger nextVal = currentVal.add(ONE);
+			if(bigInt.compareAndSet(currentVal, nextVal)) {
+				return nextVal;
+			}
+		}
+	}
+	
+	public final BigInteger getAndDecrement() {
+		while(true) {
+			BigInteger currentVal = bigInt.get();
+			BigInteger nextVal = currentVal.add(NEGATIVE_ONE);
+			if(bigInt.compareAndSet(currentVal, nextVal)) {
+				return currentVal;
+			}
+		}
+	}
+	
+	public final BigInteger decrementAndGet() {
+		while(true) {
+			BigInteger currentVal = bigInt.get();
+			BigInteger nextVal = bigInt.get().add(NEGATIVE_ONE);
 			if(bigInt.compareAndSet(currentVal, nextVal)) {
 				return nextVal;
 			}
 		}
 	}
 
+	public final BigInteger getAndDivide(BigInteger val) {
+		while(true) {
+			BigInteger currentVal = bigInt.get();
+			BigInteger nextVal = currentVal.divide(val);
+			if(bigInt.compareAndSet(currentVal, nextVal)) {
+				return currentVal;
+			}
+		}
+	}
+	
+	public final BigInteger divideAndGet(BigInteger val) {
+		while(true) {
+			BigInteger currentVal = bigInt.get();
+			BigInteger nextVal = currentVal.divide(val);
+			if(bigInt.compareAndSet(currentVal, nextVal)) {
+				return nextVal;
+			}
+		}
+	}
+	
+	public final BigInteger getAndDivideRemainder(BigInteger val) {
+		while(true) {
+			BigInteger currentVal = bigInt.get();
+			BigInteger[] result = currentVal.divideAndRemainder(val);
+			if(bigInt.compareAndSet(currentVal, result[0])) {
+				return currentVal;
+			}
+		}
+	}
+	
+	public final BigInteger[] divideRemainderAndGet(BigInteger val) {
+		while(true) {
+			BigInteger currentVal = bigInt.get();
+			BigInteger[] result = currentVal.divideAndRemainder(val);
+			if(bigInt.compareAndSet(currentVal, result[0])) {
+				return result;
+			}
+		}
+	}
+	
+	public final BigInteger getAndMultiply(BigInteger val) {
+		while(true) {
+			BigInteger currentVal = bigInt.get();
+			BigInteger nextVal = currentVal.multiply(val);
+			if(bigInt.compareAndSet(currentVal, nextVal)) {
+				return currentVal;
+			}
+		}
+	}
+	
+	public final BigInteger multiplyAndGet(BigInteger val) {
+		while(true) {
+			BigInteger currentVal = bigInt.get();
+			BigInteger nextVal = currentVal.multiply(val);
+			if(bigInt.compareAndSet(currentVal, nextVal)) {
+				return nextVal;
+			}
+		}
+	}
+	
 }
